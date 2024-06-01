94f937adf6e2a22f51e14ec50515ab5409c3e8fa
wawrzyniec <wawrzyniec.waliczek@gmail.com>
2024-06-01 20:08:59 +0200

    utworzenie planszy do gry w szachy

--- Chess/GameFrame.cpp
+++ Chess/GameFrame.cpp
@@ -1,5 +1,6 @@
 #include "GameFrame.h"
 #include "MyFrame.h"
+#include "Area.h"
 
 GameFrame::GameFrame(MyFrame* parent, const wxString& title)
 	: wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE | wxMAXIMIZE), parentFrame(parent) {
@@ -11,6 +12,26 @@ GameFrame::GameFrame(MyFrame* parent, const wxString& title)
 
 	vbox->Add(btnBackToMenu, 0, wxEXPAND | wxALL, 5);
 	vbox->Add(btnSaveGame, 0, wxEXPAND | wxALL, 5);
+
+	vbox->AddSpacer(10);
+
+	wxGridSizer* gridSizer = new wxGridSizer(8, 8, 0, 0);
+
+	int width, height;
+	GetSize(&width, &height);
+	int areaSize = std::min((width - 20) / 8, (height - 70) / 8);
+
+	for (int row = 0; row < 8; row++)
+	{
+		for (int col = 0; col < 8; col++)
+		{
+			wxPanel* areaPanel = new wxPanel(panel, wxID_ANY, wxDefaultPosition, wxSize(areaSize, areaSize), wxBORDER_SIMPLE);
+			areaPanel->SetBackgroundColour((row + col) % 2 ? *wxLIGHT_GREY : *wxYELLOW);
+			gridSizer->Add(areaPanel, 1, wxEXPAND | wxALL);
+		}
+	}
+
+	vbox->Add(gridSizer, 1, wxEXPAND | wxALL, 5);
 
 	panel->SetSizer(vbox);
 	panel->Layout();
