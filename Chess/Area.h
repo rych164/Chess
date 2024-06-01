19e88f8c693011d66987d77ce9be09ba17abe6d8
wawrzyniec <wawrzyniec.waliczek@gmail.com>
2024-06-01 19:36:43 +0200

    dodanie okienka do rozegrania partii szachów

---
+++ Chess/Area.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+
+class Area
+{
+public:
+	Area(int size, char coordinationLetters, int coordinationNumbers, const std::string& color, bool isOccupied)
+		: size(size),coordinationLetters(coordinationLetters),coordinationNumbers(coordinationNumbers),
+		color(color),isOccupied(isOccupied){}
+
+	int getSize() const { return size; }
+	char getCoordinationLetters() const { return coordinationLetters; }
+	int getCoordinationNumbers() const { return coordinationNumbers; }
+	std::string getColor() const { return color; }
+	bool getIsOccupied() const { return isOccupied; }
+
+	void setSize(int newSize) { size = newSize; }
+	void setCoordinationLetters(char newCoordinationLetters) { coordinationLetters = newCoordinationLetters; }
+	void setCoordinationNumbers(int newCoordinationNumbers) { coordinationNumbers = newCoordinationNumbers; }
+	void setColor(const std::string& newColor) { color = newColor; }
+	void setIsOccupied(bool newIsOccupied) { isOccupied = newIsOccupied; }
+
+private:
+	int size;
+	char coordinationLetters;
+	int coordinationNumbers;
+	std::string color;
+	bool isOccupied;
+};
+
