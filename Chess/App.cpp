7936f0d51a994028c8875cfc1d84cee7bbc64657
wawrzyniec <wawrzyniec.waliczek@gmail.com>
2024-06-01 17:16:00 +0200

    utworzenie prostego menu

--- Chess/App.cpp
+++ Chess/App.cpp
@@ -6,6 +6,8 @@ wxIMPLEMENT_APP(App);
 
 bool App::OnInit() {
 	MainFrame* mainFrame = new MainFrame("C++ GUI");
+	mainFrame->SetClientSize(800, 600);
+	mainFrame->Center();
 	mainFrame->Show();
 	return true;
 }
--- Chess/MainFrame.cpp
+++ Chess/MainFrame.cpp
@@ -1,6 +1,65 @@
-#include "MainFrame.h"
-#include "wx/wx.h"
+#include <wx/wx.h>
 
-MainFrame::MainFrame(const wxString& title) :wxFrame(nullptr, wxID_ANY,title){
+class MyApp : public wxApp
+{
+public: 
+	virtual bool OnInit();
+};
+class MyFrame : public wxFrame
+{
+public:
+	MyFrame(const wxString& title); 
+	void OnStartNewGame(wxCommandEvent& event);
+	void OnLoadGame(wxCommandEvent& event);
+	void OnExit(wxCommandEvent& event);
+};
 
+enum
+{
+	ID_START_NEW_GAME = 1,
+	ID_LOAD_GAME,
+	ID_EXIT,
+};
+
+bool MyApp::OnInit()
+{
+	MyFrame *frame = new MyFrame("Game Menu");
+	frame->Show(true);
+	return true;
 }
+
+MyFrame::MyFrame(const wxString& title) :wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(300, 200))
+{
+	wxPanel* panel = new wxPanel(this, wxID_ANY);
+	
+	wxBoxSizer* vbox = new wxBoxSizer(wxVERTICAL);
+
+	wxButton* startBtn = new wxButton(panel, ID_START_NEW_GAME, wxT("Start New Game"));
+	wxButton* loadBtn = new wxButton(panel, ID_LOAD_GAME, wxT("Load Game"));
+	wxButton* exitBtn = new wxButton(panel, ID_EXIT, wxT("Exit"));
+
+	vbox->Add(startBtn, 1, wxEXPAND | wxALL, 10);
+	vbox->Add(loadBtn, 1, wxEXPAND | wxALL, 10);
+	vbox->Add(exitBtn, 1, wxEXPAND | wxALL, 10);
+
+	panel->SetSizer(vbox);
+
+	Connect(ID_START_NEW_GAME, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame::OnStartNewGame));
+	Connect(ID_LOAD_GAME, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame::OnLoadGame));
+	Connect(ID_EXIT, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame::OnExit));
+}
+
+void MyFrame::OnStartNewGame(wxCommandEvent& event)
+{
+	wxMessageBox("Starting New Game");
+}
+void MyFrame::OnLoadGame(wxCommandEvent& event)
+{
+	wxMessageBox("Loading game...");
+}
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+	Close(true);
+}
+
+wxIMPLEMENT_APP(MyApp);
