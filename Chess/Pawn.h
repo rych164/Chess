7f7db151bfc18bd2e775aa06b97c031d9af48ec9
wawrzyniec <wawrzyniec.waliczek@gmail.com>
2024-06-03 12:42:36 +0200

    poprawne wyświetlanie możliwych ruchów pionków

--- Chess/Pawn.h
+++ Chess/Pawn.h
@@ -4,10 +4,12 @@
 #include <vector>
 #include <memory>
 
-class Pawn : public Piece
-{
+class Pawn : public Piece {
 public:
-    Pawn(bool isWhite, int x, int y, const std::vector<std::vector<std::unique_ptr<Piece>>>* board);
+    Pawn(bool isWhite, int x, int y, const std::vector<std::vector<std::unique_ptr<Piece>>>* boardRef);
 
     wxArrayString getPossibleMoves() const override;
+
+private:
+    const std::vector<std::vector<std::unique_ptr<Piece>>>* board;
 };
