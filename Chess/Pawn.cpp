e5f02af4090987ab312f436983c74c29259a67f0
wawrzyniec <wawrzyniec.waliczek@gmail.com>
2024-06-03 13:37:14 +0200

    Revert "poprawne wyświetlanie możliwych ruchów pionków"
    This reverts commit 7f7db151bfc18bd2e775aa06b97c031d9af48ec9.

--- Chess/Pawn.cpp
+++ Chess/Pawn.cpp
@@ -1,52 +1,33 @@
 #include "Pawn.h"
 
-Pawn::Pawn(bool isWhite, int x, int y, const std::vector<std::vector<std::unique_ptr<Piece>>>* boardRef)
-    : Piece(isWhite, x, y), board(boardRef) {}
+// Constructor initializing the Piece part and setting up a reference to the board
+Pawn::Pawn(bool isWhite, int x, int y, const std::vector<std::vector<std::unique_ptr<Piece>>>* board)
+    : Piece(isWhite, x, y)
+{
+    setBoard(board);
+}
 
 wxArrayString Pawn::getPossibleMoves() const {
     wxArrayString moves;
-    int direction = isWhite() ? -1 : 1;  // Determines movement direction based on color
-    int startRow = isWhite() ? 6 : 1;    // Determines the starting row for a double move
+    int direction = isWhite() ? 1 : -1;  // Determines movement direction based on color
+    int startRow = isWhite() ? 1 : 6;    // Determines the starting row for a double move
 
     int forwardX = posX + direction;
-    wxLogDebug("Pawn position: (%d, %d)", posX, posY);
-    wxLogDebug("Direction: %d, Start Row: %d", direction, startRow);
-
     // Check the forward move
     if (forwardX >= 0 && forwardX < 8 && !(*board)[forwardX][posY]) {
-        wxString move = wxString::Format("Move to (%d,%d)", forwardX, posY);
-        moves.Add(move);
-        wxLogDebug("Pawn forward move: %s", move);
-    }
-    else {
-        wxLogDebug("Forward move blocked at (%d, %d)", forwardX, posY);
+        moves.Add(wxString::Format("Move to (%d, %d)", forwardX, posY));
     }
 
     // Check the double step forward on the first move
     if (posX == startRow && !(*board)[forwardX][posY] && forwardX + direction >= 0 && forwardX + direction < 8 && !(*board)[forwardX + direction][posY]) {
-        wxString move = wxString::Format("Move to (%d,%d)", forwardX + direction, posY);
-        moves.Add(move);
-        wxLogDebug("Pawn double step move: %s", move);
-    }
-    else {
-        wxLogDebug("Double step move blocked at (%d, %d)", forwardX + direction, posY);
+        moves.Add(wxString::Format("Move to (%d, %d)", forwardX + direction, posY));
     }
 
     // Capture moves
     for (int dy : {-1, 1}) {
         int newY = posY + dy;
-        if (forwardX >= 0 && forwardX < 8 && newY >= 0 && newY < 8) {
-            if ((*board)[forwardX][newY] && (*board)[forwardX][newY]->isWhite() != isWhite()) {
-                wxString move = wxString::Format("Capture at (%d,%d)", forwardX, newY);
-                moves.Add(move);
-                wxLogDebug("Pawn capture move: %s", move);
-            }
-            else {
-                wxLogDebug("No capture possible at (%d, %d)", forwardX, newY);
-            }
-        }
-        else {
-            wxLogDebug("Capture move out of bounds at (%d, %d)", forwardX, newY);
+        if (forwardX >= 0 && forwardX < 8 && newY >= 0 && newY < 8 && (*board)[forwardX][newY] && (*board)[forwardX][newY]->isWhite() != isWhite()) {
+            moves.Add(wxString::Format("Capture at (%d, %d)", forwardX, newY));
         }
     }
 
--- Chess/Pawn.h
+++ Chess/Pawn.h
@@ -4,12 +4,10 @@
 #include <vector>
 #include <memory>
 
-class Pawn : public Piece {
+class Pawn : public Piece
+{
 public:
-    Pawn(bool isWhite, int x, int y, const std::vector<std::vector<std::unique_ptr<Piece>>>* boardRef);
+    Pawn(bool isWhite, int x, int y, const std::vector<std::vector<std::unique_ptr<Piece>>>* board);
 
     wxArrayString getPossibleMoves() const override;
-
-private:
-    const std::vector<std::vector<std::unique_ptr<Piece>>>* board;
 };
